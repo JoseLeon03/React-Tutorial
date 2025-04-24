c3aa6f26f126155d667fcc332b5f23bfdcef98dc
Jose Carlos Leon <jleon@traki.local>
2025-04-24 10:24:41 -0400

    Second commit

--- Clase 02/src/App.jsx
+++ Clase 02/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import { Square } from "./components/Square"
 import { TURNS } from "./constants"
 import { checkWinner } from "./logic/board"
